15714147c4e8bf9de3fbbba5438be106725631f8
youxiao <27238673@qq.com>
2018-10-06 22:56:54 +0800

    登录页面

---
+++ eUI.Model/ViewModel/LoginVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eUI.Model.ViewModel
+{
+    public class LoginVM
+    {
+        public string Account { get; set; }
+        public string Pwd { get; set; }
+        public string Vcode { get; set; }
+    }
+}
---
+++ eUI/Controllers/LoginController.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using eUI.Common;
+using eUI.Model.ViewModel;
+
+namespace easyUITest.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginVM model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    
+                    
+                        RedirectToAction("Index", "Home");
+                    
+                  
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+            }
+            return View("Error");
+        }
+
+        /// <summary>
+        /// 生产验证码
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ValidateCode()
+        {
+            ValidateCode validateCode = new ValidateCode();
+            string code = validateCode.CreateValidateCode(4);//生成验证码
+            Session["validateCode"] = code;//存储于Session中
+            byte[] buffer = validateCode.CreateValidateGraphic(code);//创建验证码的图片
+            return File(buffer, "image/jpeg");
+        }
+
+    }
+}
